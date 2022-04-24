28f4e14d185f3f043bf001825bfc84d350deefe6
seungchae <s2125@e-mirim.hs.kr>
2022-04-24 10:45:41 +0900

    bird jump

---
+++ 2022_Unity_FlappyBird/Assets/BirdJomp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdJomp : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * 3; //Vector2는 (x,y)로 나타내는 2차원 좌표 //Vector.up은 (0,1)
+        }
+    }
+}
