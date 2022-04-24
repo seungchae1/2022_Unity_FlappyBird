a498d0a649cb4c80d65a7248de53e83e3a6b266b
seungchae <s2125@e-mirim.hs.kr>
2022-04-24 12:48:53 +0900

    Pipe move

---
+++ 2022_Unity_FlappyBird/Assets/MovePipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePipe : MonoBehaviour
+{
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * Time.deltaTime * Speed; 
+        //Vector3.left = (-1,0,0)
+        //Time.deltaTime = 지난 프레임이 완료되는 데 까지 걸린 시간
+        //ex) 6프레임이면 한 프레임 당 1/6초
+    }
+}
