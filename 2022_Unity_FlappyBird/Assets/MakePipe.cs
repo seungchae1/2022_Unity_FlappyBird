15f0e0eeff97b977826c27c7fc02d560aa801ce6
seungchae <s2125@e-mirim.hs.kr>
2022-04-24 13:11:44 +0900

    pipe 생성 간격 랜덤

--- 2022_Unity_FlappyBird/Assets/MakePipe.cs
+++ 2022_Unity_FlappyBird/Assets/MakePipe.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class MakePipe : MonoBehaviour
 {
     public GameObject pipe;
+    float timer = 0;
+    float randomTime=1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(pipe);
+        timer += Time.deltaTime;
+        if (timer > randomTime)
+        {
+            Instantiate(pipe);
+            timer = 0;
+            randomTime = Random.Range(1.0f, 3.5f);
+        }
     }
 }
